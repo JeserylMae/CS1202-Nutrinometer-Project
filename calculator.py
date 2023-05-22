b0e1e9ac064d8c1f6a25f6ff195984f1dda276e4
chustinecantal <129162395+chustinecantal@users.noreply.github.com>
2023-05-22 14:33:43 +0800

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -6,6 +6,19 @@ age = int(input("Enter you age: "))
 sex = input("Enter your sex [male or female]: ")
 activity_level = input("Enter your activity level (sedentary,lightly active,moderately active,very active): ")
 
+import os
+os.system('cls')
+
+print("Input food portion a day[if none input 0]")
+num1 = float(input("Portion of meat: "))
+num2 = float(input("Portion of rice: "))
+num3 = float(input("Portion of veggies: "))
+num4 = float(input("Portion of seafood: "))
+
+totalcal = (num1 + num2 + num3 + num4)*280
+answer = totalcal / 4
+
+
 def calculate_bmr(weight, height, age, sex):
     if sex == "male":
         bmr = 66.5 + (13.8 * weight) + (5 * height) - (6.75 * age)
@@ -26,7 +39,22 @@ def calculate_daily_calories(bmr, activity_level):
 bmr = calculate_bmr(weight, height, age, sex)
 calories = calculate_daily_calories(bmr, activity_level)
 
+print("Your amount of calories is: ", answer)
 print("Your daily calorie needs are: ", calories)
+
+def Get_NormRequired():
+    if answer > calories:
+        print("\nYou are eating too many calories.")
+    elif answer == calories:
+        print("\nYou're at the right amount of calories. ")
+    else:
+        print("\nYou need to eat more with calories. ")
+
+
+
+
+
+
 
 
 
