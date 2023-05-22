7c4ccdfc789df553c374654e737690350c66fe22
reinarmedilla <123527314+reinarmedilla@users.noreply.github.com>
2023-05-22 17:46:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,3 @@
-choice = input("Enter 1 to create a new account or 2 to log in: ")
-
-if choice == "1":
-    name = input("Enter your name: ")
-    email = input("Enter your email: ")
-    password = input("Enter your password: ")
-    confPassword = input("Confirm password: ")
-
-    if password == confPassword:
-        print("User accepted!")
-        with open("user_info.txt", "a") as file:
-            file.write(f"{name},{email},{password}\n")
-
-        print("User information saved successfully!")
-    else:
-        print("Invalid password combination")
-
-elif choice == "2":
-    email = input("Enter your email: ")
-    password = input("Enter your password: ")
-
-    with open("user_info.txt", "r") as file:
-        found_user = False
-        for line in file:
-            user_info = line.strip().split(",")
-            if len(user_info) >= 3 and user_info[1] == email and user_info[2] == password:
-                found_user = True
-                print(f"Welcome back, {user_info[0]}!")
-                break
-
-        if not found_user:
-            print("Invalid email or password. Please try again.")
 import os
 import pickle
 
@@ -38,7 +6,7 @@ def getCalories(question):
     calories = input(question)
     if calories.isdigit():
         calories = int(calories)
-        if calories > -1 and calories < 50000:
+        if 0 <= calories <= 50000:
             return calories
         else:
             print("Invalid amount")
@@ -60,7 +28,7 @@ def getAweek():
     average = int(total / 7)
 
     print("\n\nYour total calorie intake for the week:", total)
-    print("\nYour average calorie intake for the week:", average)
+    print("Your average calorie intake for the week:", average)
 
     if total > 21000:
         print("\nYou are eating too many calories.")
@@ -70,12 +38,52 @@ def getAweek():
     return week
 
 
-def main():
+def create_new_account():
+    name = input("Enter your name: ")
+    email = input("Enter your email: ")
+    password = input("Enter your password: ")
+    confPassword = input("Confirm password: ")
+    if password == confPassword:
+        print("User accepted!")
+        user_data = {
+            'name': name,
+            'email': email,
+            'password': password,
+            'month': [[], [], [], []]
+        }
+        with open(f"{email}.p", "wb") as file:
+            pickle.dump(user_data, file)
+
+        print("User information saved successfully!")
+        main(user_data)
+    else:
+        print("Invalid password combination")
+
+
+def login():
+    email = input("Enter your email: ")
+    password = input("Enter your password: ")
+
+    try:
+        with open(f"{email}.p", "rb") as file:
+            user_data = pickle.load(file)
+            if user_data['password'] == password:
+                print(f"Welcome back, {user_data['name']}!")
+                return user_data
+            else:
+                print("Invalid email or password. Please try again.")
+    except FileNotFoundError:
+        print("Invalid email or password. Please try again.")
+
+    return None
+
+
+def main(user_data):
     os.system("cls" if os.name == "nt" else "clear")
     print("\t\t **********   Welcome to Nutrinometer   **********\n")
     print("Num\tMon \tTue \tWed \tThur \tFri \tSat \tSun \tAvg \tTotal")
     count = 1
-    for week in month:
+    for week in user_data['month']:
         if len(week) == 7:
             print(count, end="\t")
             for day in week:
@@ -84,19 +92,32 @@ def main():
             print(sum(week), end="\n")
         else:
             print("")
-        # print(str(count) +"\t" + "\t".join([str(x) for x in week]) + ""\n"
-        count = count + 1
+        count += 1
 
-    weeknum = int(input("\n\nWhich week would you like to edit the values for? (1,2,3,4)"))
-    month[weeknum - 1] = getAweek()
-    pickle.dump(month, open("save.p", "wb"))
+    try:
+        weeknum = int(input("\n\nWhich week would you like to edit the values for? (1,2,3,4)"))
+    except KeyboardInterrupt:
+        print("\nProgram interrupted. Exiting...")
+        return
 
-    main()
+    if 1 <= weeknum <= 4:
+        user_data['month'][weeknum - 1] = getAweek()
+        with open(f"{user_data['email']}.p", "wb") as file:
+            pickle.dump(user_data, file)
+        main(user_data)
+    else:
+        print("Invalid week number. Please try again.")
 
 
-try:
-    month = pickle.load(open("save.p", "rb"))
-except:
-    month = [[], [], [], []]
+def run_program():
+    choice = input("Enter 1 to create a new account or 2 to log in: ")
 
-main()
+    if choice == "1":
+        create_new_account()
+    elif choice == "2":
+        user_data = login()
+        if user_data:
+            main(user_data)
+
+
+run_program()
