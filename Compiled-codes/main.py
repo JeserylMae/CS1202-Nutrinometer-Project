1f4056189535962f66ed0abbec6ac13e45e14792
JESERYL MAE COMIA <128215077+JeserylMae@users.noreply.github.com>
2023-05-24 20:49:00 +0800

    Update main.py

--- Compiled-codes/main.py
+++ Compiled-codes/main.py
@@ -1,52 +1,47 @@
 """
-NOTE:
-    - install tabulate to run this program.
+NOTE: 
+    -   install tabulate to run this program.
 """
 
 import os # for clear screen
-from userlog import * # to use functions of userlog file
+from design import *
+from log import *
 
 
-def menu():
-    print(menu_h)
-    print(f"""\033[1;96m
-            [1] Sign-in
-            [2] Log-in
-            [0] Exit\n\n\033[0;96m{55*("=")}""")
+def input_CH():
+    ch = int(input("\tChoice: "))
 
-    # getting user's choice
-    while True:
-        ch = int(input("\tChoice: ")) 
-        
-        # input validation
-        try:
-            if 2 < ch or 0 > ch: # if ch is not in [0,1,2] it will direct the program to except funtion.
-                raise ValueError
-            break # this will break the loop once user entered number from 0-2.
-        except:
-            print("INVALID! Enter a Number From 0-2.")
-            print() # this will print Choice: prompt again.
+    while 2 < ch or 0 > ch: 
+        print("INVALID! Enter a Number From 0-2.")
+        input_CH()
+        break
+    return ch
 
-    ###
-    if ch == 1: # if ch is equal to 1 the user will be directed to the sign-up page.
+def CH(ch):
+    if ch == 1:
         os.system("cls")
         print(f"\n{menu_h}\n")
         sign_in()
-        
-    elif ch == 2: # if ch is equal to 2 the user will be directed to the log-in page.
+    elif ch == 2:
         os.system("cls")
         print(f"\n{menu_h}\n")
         log_in()
-
     else:
-        exit(0) # if ch is equal to 0, the program will automatically be terminated.
+        exit(0)
+
+def menu():
+    # printing header
+    print(menu_h)
+
+    # Print options
+    print(menu_options)
+
+    # choice = input(ch)
+    ch = input_CH()
+
+    #directing user's to their chosen page.
+    CH(ch)
 
 
-menu_h = (f"""\033[0;96m{55*'='}\n\033[0;31m
-        ╔═╦╗───╔╗────╔╗──────────────╔╗─────── 
-        ║║║║╔╦╗║╚╗╔╦╗╠╣╔═╦╗╔═╗╔══╗╔═╗║╚╗╔═╗╔╦╗ 
-        ║║║║║║║║╔╣║╔╝║║║║║║║║║║║║║║╩╣║╔╣║╩╣║╔╝ 
-        ╚╩═╝╚═╝╚═╝╚╝─╚╝╚╩═╝╚═╝╚╩╩╝╚═╝╚═╝╚═╝╚╝─    
-\n\033[0;96m{55*'='}""") # log-in choices
 
 menu()
